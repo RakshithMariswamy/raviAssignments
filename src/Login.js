587ae75204e88ba42c5ba4d23f7e6151c67de6a3
BC91328 <rakshith.m@macys.com>
2022-10-15 11:11:14 -0300

    first commit

---
+++ src/Login.js
@@ -0,0 +1,79 @@
+import React, {useState} from "react";
+import "./Login.css"
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+
+    const [errorMessages, setErrorMessages] = useState({});
+    let navigate = useNavigate();
+  
+    // User Login info
+    const database = [
+      {
+        username: "user1",
+        password: "pass1"
+      },
+      {
+        username: "user2",
+        password: "pass2"
+      }
+    ];
+  
+    const errors = {
+      uname: "invalid username",
+      pass: "invalid password"
+    };
+  
+    const handleSubmit = (event) => {
+      //Prevent page reload
+      event.preventDefault();
+  
+      var { uname, pass } = document.forms[0];
+  
+      // Find user login info
+      const userData = database.find((user) => user.username === uname.value);
+  
+      // Compare user info
+      if (userData) {
+        if (userData.password !== pass.value) {
+          // Invalid password
+          setErrorMessages({ name: "pass", message: errors.pass });
+        } else {
+            navigate("/listView");
+        }
+      } else {
+        // Username not found
+        setErrorMessages({ name: "uname", message: errors.uname });
+      }
+    };
+  
+    // Generate JSX code for error message
+    const renderErrorMessage = (name) =>
+      name === errorMessages.name && (
+        <div className="error">{errorMessages.message}</div>
+      );
+  
+    // JSX code for login form
+    return (
+        <div className="login-form">
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label>Username </label>
+            <input type="text" name="uname" required />
+            {renderErrorMessage("uname")}
+          </div>
+          <div className="input-container">
+            <label>Password </label>
+            <input type="password" name="pass" required />
+            {renderErrorMessage("pass")}
+          </div>
+          <div className="button-container">
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+      </div>
+    );
+}
+
+export default Login
---
+++ src/listPage.js
@@ -0,0 +1,136 @@
+import React,{useState} from 'react'
+import './App.css';
+// import mockJson from "./skill_tracker_json_structure.json"
+
+const  ListPage = () => {
+
+const [name,setName] = useState("");
+const [associateId, setAssociateId] = useState("");
+const [skill,setSkill] = useState("");
+// const [apiResponse,setApiResponse] = useState(mockJson)
+const [apiResponse,setApiResponse] = useState([])
+const selOption = [{key:"HTML-CSS-JAVASCRIPT",Value:"HTML-CSS-JAVASCRIPT"},
+{key:"ANGULAR",Value:"ANGULAR"},
+{key:"REACT",Value:"REACT"},
+{key:"SPRING",Value:"SPRING"},
+{key:"RESTFUL",Value:"RESTFUL"},
+{key:"HIBERNATE",Value:"HIBERNATE"},
+{key:"GIT",Value:"GIT"},
+{key:"DOCKER",Value:"DOCKER"},
+{key:"JENKINS",Value:"JENKINS"},
+{key:"AWS",Value:"AWS"}
+
+]
+
+const callApiToSearch = async(type) =>{
+  console.log(type)
+
+  let response = []
+  if(type === 'name'){
+     response = await fetch(`baseURL/skill-search/api/v1/admin/name/${name}`)
+  } else if(type === 'associate') {
+    response = await fetch(`baseURL/skill-search/api/v1/admin/associatedId/${associateId}`)
+  } else {
+    response = await fetch(`baseURL/skill-search/api/v1/admin/skill/${skill}`)
+  }
+  setApiResponse(response)
+}
+  
+  return (
+    <div >
+         <header className="App-header">
+      </header>
+      <div className = "searchMain">
+            <div className= "searchFields">
+               <input type="text" onChange={(e)=>setName(e.target.value)} className="inputSearch" />
+               <button onClick={()=>callApiToSearch('name')} className="buttonSearch" > Name </button>
+            </div>
+            <div className= "searchFields">
+               <input type="text" onChange={(e)=>setAssociateId(e.target.value)}  className="inputSearch"/>
+               <button onClick={()=>callApiToSearch('associate')} className="buttonSearch" > Associate ID </button>
+            </div>
+            <div className= "searchFields">
+               <select onChange={(e)=>setSkill(e.target.value)} className="inputSearch inputSelect" >
+                 {selOption.map((opt)=>{
+                    return  <option value={opt.key}>{opt.Value}</option>
+                 })}
+                </select>
+               <button onClick={()=>callApiToSearch('skill')} className="buttonSearch"> Skill </button>
+            </div>
+         </div>
+
+         {apiResponse.map((data)=>{
+            return  <div className = "displayList">
+            <div>
+               <table id="customers">
+                 <tr>
+                  <th>
+                    Name
+                  </th>
+                  <td>
+                  {data.name}
+                  </td>
+                 </tr>
+                 <tr>
+                 <th>
+                    Id
+                  </th>
+                  <td>
+                  {data.id}
+                  </td>
+                 </tr>
+                 <tr>
+                  <th>
+                    Email
+                  </th>
+                  <td>
+                  {data.email}
+                  </td>
+                 </tr>
+                 <tr>
+                  <th>
+                    Mobile
+                  </th>
+                  <td>
+                  {data.mobile}
+                  </td>
+                 </tr>
+                 
+               </table>
+            </div>
+            <div>
+            <table id="customers">
+            {data.techSkills.map((tech)=>{ 
+            return <tr>
+                  <th>
+                  {tech.skillName}
+                  </th>
+                  <td>
+                  {tech.expertiseLevel}
+                  </td>
+                 </tr>})}
+                 
+               </table>
+            </div>
+            <div>
+            <table id="customers">
+            {data.nonTechSkills.map((tech)=>{ 
+            return <tr>
+                  <th>
+                  {tech.skillName}
+                  </th>
+                  <td>
+                  {tech.expertiseLevel}
+                  </td>
+                 </tr>})}
+                 
+               </table>
+            </div>
+        </div>
+         })}
+        
+    </div>
+  );
+}
+
+export default ListPage;
